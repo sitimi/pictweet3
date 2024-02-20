2937e6f8b2cee027b23f5aaf5abf7a3bdde1db2f
sitimi <sitimi522@gmail.com>
2024-02-20 20:32:43 +0900

    createアクション定義＆ストロングパラメーター設定

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,4 +7,15 @@ class TweetsController < ApplicationController
     @tweet = Tweet.new
   end
 
+  def create
+    Tweet.create(tweet_params)
+    redirect_to '/'
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:name, :image, :text)
+  end
+
 end
