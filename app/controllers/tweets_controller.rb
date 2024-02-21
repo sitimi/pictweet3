82989b1558f251f6213400687426168b8dafa3ba
sitimi <sitimi522@gmail.com>
2024-02-21 23:42:25 +0900

    未ログイン時の転送設定

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,7 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:edit, :show]
+  before_action :move_to_index, except: [:index, :show]
+
   def index
     @tweets = Tweet.all
   end
@@ -39,6 +41,12 @@ class TweetsController < ApplicationController
 
   def tweet_params
     params.require(:tweet).permit(:name, :image, :text)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
 end
